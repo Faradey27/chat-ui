341477070c8b127177725bf49577b27095a658b9
Faradey27 <andrii.tiertyshnyi@gmail.com>
2018-02-28 20:56:39 +0200

    Updated project structure:  - each component has own folder with styles, tests, own subcomponents, etc.  - containers and components approach(clever and dump components)  - services folder, for some mostly independent batches of utils  - screenshots, comments, resources files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './containers/App';
+import registerServiceWorker from './services/pwa/registerServiceWorker';
+
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+/*
+  Here we have entry point for react app,
+  we mount our root component - App on dom node.
+*/
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+/*
+  As we want to be PWA app, we need to become offline friendly, for this reason we register service worker
+*/
 registerServiceWorker();
